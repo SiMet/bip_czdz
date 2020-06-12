92b9b4de5c6155bac0353993229ba2e49c2f8d6a
Marek Szkowron <marek.szkowron@comarch.com>
2020-06-13 01:19:36 +0200

    Add eurzad pages to parse, split into separate files

--- bip.py
+++ bip.py
@@ -25,26 +25,15 @@ links = [
 'https://www.bip.czechowice-dziedzice.pl/bipkod/008/132', # Obwieszczenia PREZYDENTA BIELSKA-BIAŁEJ
 ]
 
-epuap = {
-    "zaproszenia na RM": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+projekt%C3%B3w+uchwa%C5%82+Rady+Miejskiej&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Zamowienia publiczne": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%284%29',
-    "Zamowienia publiczne do 30k EUR": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+zam%C3%B3wie%C5%84+publicznych+do+30+000+EURO&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%283%29',
-    "Komisja skarg": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+Komisji+Skarg+Wniosk%C3%B3w+i+Petycji+-+VIII+kadencja&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Komisja Budzetu i finansow": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+posiedze%C5%84+Komisji+Bud%C5%BCetu+i+Finans%C3%B3w&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Komisja Oswiaty, kultury i sportu": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+posiedze%C5%84+Komisji+O%C5%9Bwiaty%2C+Kultury+i+Sportu&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Komisji Polityki Spolecznej": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+posiedze%C5%84+Komisji+Polityki+Spo%C5%82ecznej&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Komisji Rewizyjnej": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+posiedze%C5%84+Komisji+Rewizyjnej&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Komisji Rozwoju i Rolnictwa": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+posiedze%C5%84+Komisji+Rozwoju+i+Rolnictwa&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Komisji Samorzadnosci i Porzadku Publicznego": 'https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Rejestr+protoko%C5%82%C3%B3w+z+posiedze%C5%84+Komisji+Samorz%C4%85dno%C5%9Bci+i+Porz%C4%85dku+Publicznego&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%282%29',
-    "Obwieszczenia o wszczetych postepowaniach": "https://gmczechowicedziedzice.peup.pl/eurzad.seam?eurzadNazwa=Obwieszczenia+o+wszcz%C4%99tych+post%C4%99powaniach&actionMethod=eurzad.xhtml%3ApeupAgent.setEurzadMode%283%29"
-}
-
 import urllib.request as urllib2
+import urllib.error
 #from HTMLParser import HTMLParser
 from html.parser import HTMLParser
 import unicodedata
 import json
 import datetime
+from bip_eurzad_utils import get_zamowienia_publiczne, get_rejestry
+from bip_peup import get_from_peup
 
 class MyHTMLParser(HTMLParser):
 
@@ -90,60 +79,6 @@ class MyHTMLParser(HTMLParser):
             if not self.section["title"]:
                 self.section["title"] = str(unicodedata.normalize("NFKD", data.strip()).encode('ascii','ignore'))
 
-class TableParser(HTMLParser):
-
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.tr = False
-        self.td = False
-        self.begin_of_td_data = ""
-        self.begin_of_td_data_finished = False
-        self.table = False
-        self.a = False
-        self.section = None
-        self.section_found = None
-
-    def get_attr_value(self, attrs, name):
-        return next(iter([attr[1] for attr in attrs if attr[0]==name]), None)
-
-    def handle_starttag(self, tag, attrs):
-        _class =self.get_attr_value(attrs, "class")
-        if tag == "table" and _class and "rich-table" in _class:
-            self.table = True
-        if tag == "tr":
-            self.tr = True
-        if tag == "td":
-            self.td = True
-        if tag == "a":
-            href = self.get_attr_value(attrs, "href")
-            self.begin_of_td_data_finished = True
-            if "zalId" in href:
-                self.section = {"id": str(href), "title": str(self.begin_of_td_data+": \n"), "v": str(href)}
-                self.a = True
-
-    def handle_endtag(self, tag):
-        if tag == "tr":
-            self.tr = False
-        if tag == "td":
-            self.td = False
-            self.begin_of_td_data_finished = False
-            self.begin_of_td_data = ""
-        if tag == "table":
-            self.table = False
-        if tag == "a":
-            self.a = False
-            if self.section:
-                self.section_found(self.section)
-                self.section = None
-
-    def handle_data(self, data):
-        if self.table and self.tr and data.strip():
-            if self.td and not self.begin_of_td_data_finished:
-                self.begin_of_td_data += str(unicodedata.normalize("NFKD", data.strip()).encode('ascii','ignore'))
-            if self.section:
-                if self.a:
-                    #print data
-                    self.section["title"] += str(unicodedata.normalize("NFKD", data.strip()).encode('ascii','ignore'))
 def section_found(s, bip, href):
     print(s)
     if s["id"] not in bip or bip[s["id"]] != s["v"]:
@@ -154,6 +89,15 @@ def section_found(s, bip, href):
         f.write("{} On page: {} new version:\n{}\r\n".format(today_str, href, s["title"]))
         f.close() 
 
+def new_eurzad_doc_found(doc, bip):
+    if doc["id"] not in bip or bip[doc["id"]] != doc["v"]:
+        bip[doc["id"]] = doc["v"]
+        f = open("bip_new_found.txt","a+")
+        f.write("{} On page: {} new version:\n{}\r\n".format(doc["data"], doc["href"], doc["title"]))
+        f.close() 
+        print("New {} {} {}".format(doc["id"], doc["title"], doc["v"]))
+
+
 bip = {}
 try:
     with open('bip_docs.txt', 'r') as f:
@@ -162,7 +106,7 @@ except IOError:
     print("Could not open bip_docs.txt")
 
 for href in links:
-    #continue
+    continue
     print("GET {}".format(href))
     response = urllib2.urlopen(href)
     html = response.read()
@@ -171,21 +115,32 @@ for href in links:
     parser.section_found = lambda s: section_found(s, bip, href)
     parser.feed(html.decode("utf-8"))
 
-response = urllib2.urlopen('https://gmczechowicedziedzice.peup.pl/eurzad.seam')
-html = response.read()
-cookie = response.headers.get('Set-Cookie')
+# get_from_peup(section_found, bip)
 
-for title, href in epuap.items():
-    print("GET {} {}".format(title, href))
-    request = urllib2.Request(href)
-    request.add_header("cookie", cookie)
-    response = urllib2.urlopen(request)
-    html = response.read()
-    #print html
-    parser = TableParser()
-    parser.section_found = lambda s: section_found(s, bip, title+" "+href)
-    parser.feed(html.decode("utf-8"))
+try:
+    get_zamowienia_publiczne(20, lambda d: new_eurzad_doc_found(d, bip))
+    get_rejestry(
+        {
+            "brmrpuVIII": {"count": 1},  # Zaproszenia na RM
+            "ua_odecyzj": {"count": 15},  # Obwieszczenia o wydanych decyzjach
+            "ua_opostep": {"count": 5},  # Obwieszczenia o wszczętych postępowaniach
+            "or_rzb": {"count": 15},  # Rejestr zarządzeń Burmistrza
+            "brm_ksipVI": {"count": 2},  # Rejestr protokołów z posiedzeń Komisji Samorządności i Porządku Publicznego
+            "brm_krirVI": {"count": 2},  # Rejestr protokołów z posiedzeń Komisji Rozwoju i Rolnictwa
+            "brm_krVI": {"count": 2},  #  Rejestr protokołów z posiedzeń Komisji Rewizyjnej
+            "brm_kpsVI": {"count": 2},  #  Rejestr protokołów z posiedzeń Komisji Polityki Społecznej
+            "brm_kkisVI": {"count": 2},  #  Rejestr protokołów z posiedzeń Komisji Oświaty, Kultury i Sportu
+            "brm_kbifVI": {"count": 2},  #  Rejestr protokołów z posiedzeń Komisji Budżetu i Finansów
+            "brm_swipVIII": {"count": 2}  #  Rejestr protokołów z Komisji Skarg Wniosków i Petycji - VIII kadencja
+        },
+        lambda d: new_eurzad_doc_found(d, bip)
+    )
+except urllib.error.URLError as e:
+    print("Exception during fetch eurzad")
+    f = open("bip_new_found.txt","a+")
+    f.write("Exception during fetch eurzad - {}\n".format(e.reason))
+    f.close() 
 
 with open('bip_docs.txt', 'w') as outfile:
-    print(bip)
+    # print(bip)
     json.dump(bip, outfile)
---
+++ bip_eurzad_utils.py
@@ -0,0 +1,77 @@
+import urllib.request
+import json
+from datetime import datetime, timedelta
+
+
+def make_request(method, params):
+    url = "https://eurzad.finn.pl/gmczechowicedziedzice/"+method
+    body = {"id":1,"method": method, "params": params,"jsonrpc":"2.0"}
+    req = urllib.request.Request(url)
+
+    jsondata = json.dumps(body)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+    req.add_header('Content-Length', len(jsondataasbytes))
+    f = urllib.request.urlopen(req, jsondataasbytes, timeout=5)
+    response = f.read()
+    f.close()
+    return response.decode('utf8')
+
+def get_from_result(data):
+    try:
+        return json.loads(json.loads(data)["result"])
+    except TypeError as e:
+        print("\n\nException while parse response: \n{}\n".format(data))
+        raise e
+
+def get_from_lista(data):
+    return get_from_result(data)["lista"]
+
+def get_zamowienie(id, tytul, callback):
+    method = "/kv/routeZamowieniaServiceManager1"
+    response = make_request(method, [str(id)])
+    data = get_from_result(response)
+    for zal in data["zalaczniki"]:
+        print("    Załącznik {}".format(zal["nazwa"]))
+        zal_id = "/pobierz.seam?zbior=3&plikId={}&zalId={}".format(id, zal["id"])
+        callback({"id": zal_id, "v": zal_id, "title": tytul +"\n"+zal["nazwa"], "data": zal["data"], "href": "https://eurzad.finn.pl/gmczechowicedziedzice/#!/zamowienie/"+str(id)})
+
+def get_zamowienia_publiczne(count, callback):
+    def_filter = json.dumps({"szukaj":None,"rodzaj":None,"dataOd":None,"dataDo":None,"terminOd":None,"terminDo":None,"dodatkowe":[],"skorowidz":None})
+    def_param = [str(count), "1", "\"DEFAULT\"", def_filter]
+
+    method = "/kv/routeZamowieniaServiceManager0"
+    response = make_request(method, def_param)
+    zamowienia_publiczne = get_from_lista(response)
+
+    for z in zamowienia_publiczne:
+        print("{} {}".format(z["id"], z["temat"]))
+        get_zamowienie(z["id"], z["temat"], callback)
+
+def get_rejestr_entry(id, tytul, callback):
+    method = "/kv/routeRejestryServiceManager2"
+    response = make_request(method, [str(id)])
+    data = get_from_result(response)
+    for zal in data["zalaczniki"]:
+        print("    Załącznik {}".format(zal["nazwa"]))
+        zal_id = "/pobierz.seam?zbior=1&plikId={}&zalId={}".format(id, zal["id"])
+        callback({"id": zal_id, "v": zal_id, "title": tytul +"\n"+zal["nazwa"], "data": zal["data"], "href": "https://eurzad.finn.pl/gmczechowicedziedzice/#!/rejestr/"+str(id)})
+
+def get_rejestry(przegladane_rejestry, callback):
+    method = "/kv/routeRejestryServiceManager0"
+    rejestry = get_from_result(make_request(method, []))
+    for rej in rejestry:
+        if rej["symbol"] not in przegladane_rejestry:
+            continue
+        count = przegladane_rejestry[rej["symbol"]]["count"]
+        print(rej["symbol"] + " "+ rej["nazwa"])
+        tomorrow = datetime.now()+ timedelta(days=1)
+        tomorrow_str = tomorrow.strftime("%d/%m/%YT%H:%M:%S.0")
+        def_filter = json.dumps({"szukaj":None,"prawo":None,"realizacja":None,"dataWydOd":None,"dataWydDo":tomorrow_str,"dataOglOd":None,"dataOglDo":None,"dataWOd":None,"dataWDo":None,"dodatkowe":[],"skorowidz":None})
+        response = json.loads(make_request("/kv/routeRejestryServiceManager1", [rej["symbol"], count, 1, "\"DEFAULT\"", def_filter]))
+        result = json.loads(response["result"])
+        for r in result["lista"]:
+            print("{} {} {}".format(r["id"], r["dataW"], r["temat"]))
+            get_rejestr_entry(r["id"], r["temat"], callback)
+            
+        print("  ")
